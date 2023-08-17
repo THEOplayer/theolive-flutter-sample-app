60d52f183f1a8cfb1be3887894814722142866e2
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-08-17 14:36:49 +0200

    added Compose compatibility

--- android/app/src/main/kotlin/live/theo/theolive_flutter_sample/MainActivity.kt
+++ android/app/src/main/kotlin/live/theo/theolive_flutter_sample/MainActivity.kt
@@ -1,6 +1,11 @@
 package live.theo.theolive_flutter_sample
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
 }
