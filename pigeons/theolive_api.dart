93b87d8e4c4bbd68fa113266984b576605d1a0b2
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-08-17 15:50:39 +0200

    added pigeon interface and generated classes

---
+++ pigeons/theolive_api.dart
@@ -0,0 +1,29 @@
+import 'package:pigeon/pigeon.dart';
+
+//run in the root folder: flutter pub run pigeon --input pigeons/theolive_api.dart
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/pigeon/theolive_api.g.dart',
+  dartOptions: DartOptions(),
+  kotlinOut: 'android/app/src/main/kotlin/live/theo/theolive_flutter_sample/bindings/pigeon/THEOliveAPI.g.kt',
+  kotlinOptions: KotlinOptions(
+    package: 'live.theo.theolive_flutter_sample.bindings.pigeon'
+  ),
+  swiftOut: 'ios/Runner/Bindings/pigeon/THEOliveAPI.g.swift',
+  swiftOptions: SwiftOptions(),
+  dartPackageName: 'theolive_flutter_sample',
+))
+
+
+//Talking to the native
+@HostApi()
+abstract class THEOliveNativeAPI {
+  void loadChannel(String channelID);
+}
+
+//Native talks to Dart
+
+@FlutterApi()
+abstract class THEOliveFlutterAPI {
+  void onChannelLoadedEvent(String channelID);
+}
