d7472b4c1e15553cf8a1bf2afaf67ac0637876fc
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-09-06 22:17:38 +0200

    change THEOliveView to a stateful widget

--- lib/theolive_view.dart
+++ lib/theolive_view.dart
@@ -5,14 +5,44 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:theolive_flutter_sample/theolive_view_controller.dart';
 
-class THEOliveView extends StatelessWidget {
+class THEOliveView extends StatefulWidget {
 
   final Function(THEOliveViewController) onTHEOliveViewCreated;
 
-  const THEOliveView({required Key key, required this.onTHEOliveViewCreated,}) : super(key: key);
+  THEOliveView({required Key key, required this.onTHEOliveViewCreated,}) : super(key: key);
+
+  late THEOliveViewController viewController;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _THEOliveViewState();
+  }
+
+}
+
+class _THEOliveViewState extends State<THEOliveView> {
+
+  late THEOliveViewController viewController;
+
+  @override
+  void initState() {
+    print("_THEOliveViewState initState");
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    print("_THEOliveViewState dispose");
+    // NOTE: this would be nicer, if we move it inside the THEOliveView that's a StatefulWidget
+    // FIX for https://github.com/flutter/flutter/issues/97499
+    viewController.manualDispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    print("_THEOliveViewState build");
+
     // This is used in the platform side to register the view.
     const String viewType = 'theoliveview';
     // Pass parameters to the platform side.
@@ -26,7 +56,8 @@ class THEOliveView extends StatelessWidget {
               (context, controller) {
             return AndroidViewSurface(
               controller: controller as AndroidViewController,
-              gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{},
+              gestureRecognizers: const <Factory<OneSequenceGestureRecognizer>>{
+              },
               hitTestBehavior: PlatformViewHitTestBehavior.opaque,
             );
           },
@@ -42,8 +73,11 @@ class THEOliveView extends StatelessWidget {
               },
             )
               ..addOnPlatformViewCreatedListener((id) {
+                print("_THEOliveViewState OnPlatformViewCreatedListener");
                 params.onPlatformViewCreated(id);
-                onTHEOliveViewCreated(THEOliveViewController(id));
+                viewController = THEOliveViewController(id);
+                widget.viewController = viewController;
+                widget.onTHEOliveViewCreated(viewController);
               })
               ..create();
           },
@@ -55,21 +89,38 @@ class THEOliveView extends StatelessWidget {
             creationParams: creationParams,
             creationParamsCodec: const StandardMessageCodec(),
             onPlatformViewCreated: (id) {
-              onTHEOliveViewCreated(THEOliveViewController(id));
+              print("_THEOliveViewState OnPlatformViewCreatedListener");
+              viewController = THEOliveViewController(id);
+              widget.viewController = viewController;
+              widget.onTHEOliveViewCreated(viewController);
             }
         );
       default:
         return Text("Unsupported platform $defaultTargetPlatform");
     }
-
-
   }
 
-  void _onPlatformViewCreated(int id) {
-    if (onTHEOliveViewCreated == null) {
-      return;
-    }
-    onTHEOliveViewCreated(THEOliveViewController(id));
+  @override
+  void didChangeDependencies() {
+    print("_THEOliveViewState didChangeDependencies");
+    super.didChangeDependencies();
   }
 
+  @override
+  void activate() {
+    print("_THEOliveViewState activate");
+    super.activate();
+  }
+
+  @override
+  void deactivate() {
+    print("_THEOliveViewState deactivate");
+    super.deactivate();
+  }
+
+  @override
+  void reassemble() {
+    print("_THEOliveViewState reassemble");
+    super.reassemble();
+  }
 }
