c40879f6bcebf978cb0c78110259286582550eaf
Daniel Dallos <daniel.dallos@theoplayer.com>
2023-09-09 00:16:52 +0200

    wait for the playing event to show the player

--- android/app/src/main/kotlin/live/theo/theolive_flutter_sample/bindings/THEOliveView.kt
+++ android/app/src/main/kotlin/live/theo/theolive_flutter_sample/bindings/THEOliveView.kt
@@ -6,13 +6,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.platform.ViewCompositionStrategy
-import com.theolive.player.api.*
+import com.theolive.player.api.EventListener
+import com.theolive.player.api.THEOliveChromeless
+import com.theolive.player.api.THEOlivePlayer
+import com.theolive.player.api.rememberTHEOlivePlayer
 import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.platform.PlatformView
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
 import live.theo.theolive_flutter_sample.bindings.pigeon.THEOliveFlutterAPI
 import live.theo.theolive_flutter_sample.bindings.pigeon.THEOliveNativeAPI
 
@@ -28,6 +45,14 @@ class THEOliveView(context: Context, viewId: Int, args: Any?, messenger: BinaryM
     private val flutterApi: THEOliveFlutterAPI
 
     private var loadChannelJob: Deferred<Unit>? = null
+
+
+    // Workaround to eliminate the inital transparent layout with ComposeView
+    // TODO: remove it once Compose is not used.
+    private var _isFirstPlaying = mutableStateOf(false);
+    private var isFirstPlaying: Boolean
+        get() = _isFirstPlaying.value
+        set(value) { _isFirstPlaying.value = value }
 
     init {
         Log.d("THEOliveView", "init $viewId");
@@ -62,7 +87,23 @@ class THEOliveView(context: Context, viewId: Int, args: Any?, messenger: BinaryM
                 loadChannelJob!!.start()
             }
 
-            THEOliveChromeless(modifier = Modifier.fillMaxSize(), player = player)
+
+            // workaround to hide the player until it is not playing.
+            // player has to be present in the view hierarchy to start loading, so we can't dynamically add it for now
+            // TODO: remove it once Compose is not used.
+            val visibility by remember {
+                derivedStateOf {
+                    if (isFirstPlaying) {
+                        1.0F
+                    } else {
+                        0.0F
+                    }
+                }
+            }
+
+            Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
+                THEOliveChromeless(modifier = Modifier.fillMaxSize().alpha(visibility), player = player)
+            }
 
         }
 
@@ -86,6 +127,11 @@ class THEOliveView(context: Context, viewId: Int, args: Any?, messenger: BinaryM
 
     override fun onPlaying() {
         Log.d("THEOliveView", "onPlaying");
+
+        if (!isFirstPlaying) {
+            isFirstPlaying = true
+        }
+
         CoroutineScope(Dispatchers.Main).launch {
             flutterApi.onPlaying {
                 Log.d("THEOliveView", "JAVA onPlaying ack received: ")
@@ -111,6 +157,7 @@ class THEOliveView(context: Context, viewId: Int, args: Any?, messenger: BinaryM
 
     override fun onReset() {
         Log.d("THEOliveView", "onReset");
+        isFirstPlaying = false
     }
 
     override fun onError(message: String) {
